315550df2eee1343ead98248340145168d35cc9d
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-26 15:55:51 +0530

    MODIFIED: Added flip coin simulation for singlet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,11 +2,54 @@
 # Constants
 IS_HEADS=0
 IS_TAILS=1
-# To generate random and print heads or tails accordingly
-outcomeCheck=$((RANDOM%2))
-if(($outcomeCheck==$IS_HEADS))
+HEADS=0
+TAILS=1
+#variables
+count1=0
+count2=0
+count3=0
+count4=0
+count5=0
+count6=0
+count7=0
+count8=0
+count9=0
+count10=0
+count11=0
+count12=0
+count13=0
+count14=0
+declare -A coinDictionary
+coinDictionary=([H]=$count1 [T]=$count2)
+
+# Function to generate random and return heads or tails accordingly
+function getFlipCoinResult()
+{
+	outcomeCheck=$((RANDOM%2))
+	if(($outcomeCheck==$IS_HEADS))
+		then
+			echo $HEADS 
+		else
+			echo $TAILS
+	fi
+}
+read -p "How many times do you want to flip a coin?:" noOfFlips
+for((flipCount=1;flipCount<=$noOfFlips;flipCount++))
+do
+	if(("$(getFlipCoinResult)"==$HEADS))
 	then
-		echo "Heads"
+			coinDictionary[H]=$((${coinDictionary[H]}+1))
 	else
-		echo "Tails"
-fi
+			coinDictionary[T]=$((${coinDictionary[T]}+1))
+	fi
+done
+# Print the dictionary of singlet combination
+for key in "${!coinDictionary[@]}"
+do
+	echo ""${key}" : "${coinDictionary[$key]}"" 
+done
+
+headPercentage=`echo " scale = 4 ; (${coinDictionary[H]}*100)/$noOfFlips" | bc -l`
+tailPercentage=`echo " scale = 4 ; 100-$headPercentage" | bc -l`
+echo "Percentage of heads is :" $headPercentage
+echo "Percentage of tails is :" $tailPercentage
